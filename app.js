38a2dd264a9940830c59fa65b5975639d9482472
Payad <swordoflight8847@yahoo.com>
2023-08-03 14:58:42 -0700

    styling page for media queries

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const fetchCharData = () => {
     const characters = [];
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i < 5; i++) {
     // const characters = [];
     const url = (`https://swapi.dev/api/people/${i}`);
     characters.push(fetch(url).then((data) => data.json()))
@@ -39,7 +39,7 @@ Promise.all(characters).then((results) => {
 // }
 
 const planets = [];
-for (let i = 1; i < 3; i++) {
+for (let i = 1; i < 10; i++) {
     const url = `https://swapi.dev/api/planets/${i}`;
     planets.push(fetch(url).then((data) => data.json()));
 }
@@ -93,16 +93,21 @@ const createCharacter = (characters) => {
     console.log(characterDiv)
     const charBtn = document.querySelector('.peopleBtn');
     const filmsDiv = document.querySelector('.Films');
+    const planetDiv = document.querySelector('.Planets');
+    const starshipDiv = document.querySelector('.Starships');
 
     charBtn.addEventListener('click', () => {
     filmsDiv.innerHTML = '';
+    planetDiv.innerHTML = '';
+    starshipDiv.innerHTML = '';
+    
     const characterSection = characters.map((character) => 
-        `<div>
-            <div>${character.name}</div>
-            <div>${character.height}</div>
-            <div>${character.hair_color}</div>
-            <div>${character.weight}</div>
-            <div>${character.birth_year}</div>
+        `<div class="characterDiv">
+            <div class="name info">${character.name}</div>
+            <div class="height character info">Height <span id="character">${character.height}</span></div>
+            <div class="hair_color character info">Hair Color <span id="character">${character.hair_color}<span></div>
+            <div class="weight character info">Weight <span id="character">${character.weight}</span></div>
+            <div class="birth_year character info">Birth Year <span id="character">${character.birth_year}</span></div>
         </div>`
     ).join('');
 
@@ -114,14 +119,18 @@ const createFilms = (films) => {
     const filmsDiv = document.querySelector('.Films');
     const filmsBtn = document.querySelector('.filmsBtn');
     const characterDiv = document.querySelector('.Characters');
+    const planetDiv = document.querySelector('.Planets');
+    const starshipDiv = document.querySelector('.Starships');
 
     filmsBtn.addEventListener('click', () => {
+    planetDiv.innerHTML = '';
+    starshipDiv.innerHTML = '';
     characterDiv.innerHTML = '';
     const Films = films.map((film) => 
-        `<div>
-            <div>${film.title}</div>
-            <div>${film.episode}</div>
-            <div>${film.opening_crawl}</div>
+        `<div class="film">
+            <div class="title">${film.title}</div>
+            <div class="episode">Episode: <span id="film">${film.episode}</span></div>
+            <div class="description">${film.opening_crawl}</div>
             
         </div>`
     )
@@ -135,21 +144,23 @@ const createPlanets = (planets) => {
     const characterDiv = document.querySelector('.Characters');
     const filmsDiv = document.querySelector('.Films');
     const planetsBtn = document.querySelector('.planetsBtn');
+    const starshipDiv = document.querySelector('.Starships');
 
     planetsBtn.addEventListener('click', () => {
         characterDiv.innerHTML = '';
         filmsDiv.innerHTML = '';
+        starshipDiv.innerHTML = '';
     const Planets = planets.map((planet) => 
-        `<div>
-        <div>${planet.name}</div>
-        <div>${planet.rotation_period}</div>
-        <div>${planet.orbital_period}</div>
-        <div>${planet.diameter}</div>
-        <div>${planet.climate}</div>
-        <div>${planet.gravity}</div>
-        <div>${planet.terrain}</div>
-        <div>${planet.surface_water}</div>
-        <div>${planet.population}</div>
+        `<div class="planetDiv">
+        <div class="planetName">Name <span>${planet.name}</span></div>
+        <div class="rotation">Rotation period <span class="info">${planet.rotation_period}</span></div>
+        <div class="orbital_period">Orbital period <span class="info">${planet.orbital_period}</span></div>
+        <div class="diameter">Diameter <span class="info">${planet.diameter}</span></div>
+        <div class="climate">Climate <span class="info">${planet.climate}</span></div>
+        <div class="gravity">Gravity <span class="info">${planet.gravity}</span></div>
+        <div class="terrain">Terrain <span class="info">${planet.terrain}</span></div>
+        <div class="surface_water">Surface Water <span class="info">${planet.surface_water}<span></div>
+        <div class="population">Population <span class="info">${planet.population}</span></div>
         </div>`
     ).join('');
 
@@ -169,18 +180,18 @@ const createStarships = (starships) => {
         characterDiv.innerHTML = '';
         filmsDiv.innerHTML = '';
     const Starships = starships.map((starship) => 
-        `<div>
-        <div>${starship.name}</div>
-        <div>${starship.model}</div>
-        <div>${starship.manufacturer}</div>
-        <div>${starship.max_atmosphering_speed}</div>
-        <div>${starship.crew}</div>
-        <div>${starship.passengers}</div>
-        <div>${starship.cargo_capacity}</div>
-        <div>${starship.consumables}</div>
-        <div>${starship.hyperdrive_rating}</div>
-        <div>${starship.MGLT}</div>
-        <div>${starship.starship_class}</div>
+        `<div class="starship">
+        <div class="starship_name">${starship.name}</div>
+        <div class="model">Model: <span id="ship_info">${starship.model}</span></div>
+        <div class="manufacturer">Manufacturer: <span id="ship_info">${starship.manufacturer}</span></div>
+        <div class="atmosphering_speed">Atmosphering speed: <span id="ship_info">${starship.max_atmosphering_speed}</span></div>
+        <div class="crew">Crew: <span id="ship_info">${starship.crew}</span></div>
+        <div class="passengers">Passengers: <span id="ship_info">${starship.passengers}</span></div>
+        <div class="cargo">Cargo_capacity: <span id="ship_info">${starship.cargo_capacity}</span></div>
+        <div class="consumables">Consumables: <span id="ship_info">${starship.consumables}</span></div>
+        <div class="hyperdrive">Hyperdrive rating: <span id="ship_info">${starship.hyperdrive_rating}</span></div>
+        <div class="MGLT">MGLT <span id="ship_info">${starship.MGLT}</span></div>
+        <div class="starship_class">Starship class: <span id="ship_info">${starship.starship_class}</span></div>
         </div>`
     ).join('');
     starshipDiv.innerHTML = Starships;
